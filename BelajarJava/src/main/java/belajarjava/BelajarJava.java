3130d2540411d11272dceab130a377764a817f07
Raaa-ux <113575196+Raaa-ux@users.noreply.github.com>
2024-08-22 15:02:36 +0700

    Add files via upload

---
+++ BelajarJava/src/main/java/belajarjava/BelajarJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package belajarjava;
+
+/**
+ *
+ * @author DINI
+ */
+public class BelajarJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
